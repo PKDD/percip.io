0a7931356a4f2f270bf3c8b9d7f7018d7e0f2272
Peter Kunick <kunick@itisim.com>
2016-04-05 08:49:15 +0200

    Filter button for todays date

--- src/report/main.js
+++ src/report/main.js
@@ -1,7 +1,22 @@
 ﻿$('document').ready(function(){
 
 	$('table').find('tr').filter(':even').addClass('striped');
+	var today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth()+1; //January is 0!
+var yyyy = today.getFullYear();
+
+if(dd<10) {
+    dd='0'+dd
+}
+
+if(mm<10) {
+    mm='0'+mm
+}
+
+today = dd+'.'+mm+'.'+yyyy;
 	$('table').filterTable({
+		quickList:[today],
     callback: function(term, table) {
         table.find('tr').removeClass('striped').filter(':visible:even').addClass('striped');
     }
