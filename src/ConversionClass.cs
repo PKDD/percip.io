da7aa6c124c4c296c8d54b7d47607faba03ae050
Peter Kunick <kunick@itisim.com>
2016-04-06 09:34:44 +0200

    Created ConversionClass

---
+++ src/ConversionClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace percip.io
+{
+    class ConversionClass
+    {
+        public void RenewData<T>(IDataSaver ToRepair, string filename)
+        {
+            T obj = ToRepair.Load<T>(filename);
+
+            ToRepair.Save(filename, obj);
+        }
+    }
+}
