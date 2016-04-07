9b8e0ba99aeb20e2cfb13ce7c1995419705c30c1
Peter Kunick <kunick@itisim.com>
2016-04-07 10:22:07 +0200

    repair

--- src/Properties/Settings.Designer.cs
+++ src/Properties/Settings.Designer.cs
@@ -34,6 +34,9 @@ namespace percip.io.Properties {
                 this["lastenergysave"] = value;
             }
         }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.DefaultSettingValueAttribute("True")]
         public bool Protected {
             get {
